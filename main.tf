b1e0bded86d931743741441ba682816e0ce8f2de
mahesh6419 <saimahesh6419@gmail.com>
2026-02-28 13:12:50 +1100

    corrected the default value

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main"{
     cidr_block = var.vpc_cidr
     enable_dns_support = true
     enable_dns_hostnames = true
-    instance_tenancy = default
+    instance_tenancy = "default"
 
   tags = local.vpc_final_tags
   
