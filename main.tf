583f6155ab345d285d1cf70362278525fe04e5e6
mahesh6419 <saimahesh6419@gmail.com>
2026-02-28 12:47:09 +1100

    created vpc:cidr,finaltags

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "main"{
+    cidr_block = var.vpc_cidr
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    instance_tenancy = default
+
+  tags = local.vpc_final_tags
+  
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    description = "The name of the project."
+    type        = string
+  
+}
+variable "environment" {
+  description = "The name of the environment."
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+  
+}
+
+variable "vpc_tags"{
+    description = "Additional tags to apply to the VPC."
+    type        = map(string)
+    default     = {}
+}
+  
